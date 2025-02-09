f92cfb1189201d5c3afb4eeb1c243e407e2cf0a6
Pooja jat <patelpoojajat@gmail.com>
2025-02-09 19:13:51 +0530

    Update

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-
-
 function updateClock() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
-
-
-  const timeString = hours + ":" + minutes + ":" + seconds;
+  const timeString = `${hours} : ${minutes} : ${seconds}`;
   document.getElementById("clock-container").innerText = timeString;
 }
+// const now = new Date();
+// const hours = now.getHours();
 setInterval(updateClock, 1000);
-updateClock();
 
-
-
-
+const setPm = () => {
+  let now = new Date();
+  let hours = now.getHours();
+  let p = document.querySelector("p");
+  p.innerText = hours > 12 ? "PM" : "AM";
+};
+setPm();
